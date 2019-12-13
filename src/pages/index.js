eed01124c12e5060aa7d44d43cc3ad1a7975bdd4
Samuel Brehm <samuelbrehm@gmail.com>
2019-12-13 17:07:36 -0300

    Build main layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import PostItem from "../components/PostItem"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home</h1>
+    <PostItem
+        slug="/about/"
+        background="green"
+        category="Misc"
+        date="23 de agosto de 2019"
+        timeToRead="4 min"
+        title="Veja nosso novo trabalho realizado"
+        description="Apresentamos o blog da Arte e Mimo. Descubra o melhor da internet e mostre suas ideias."
+    />
   </Layout>
 )
 
